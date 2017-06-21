c5af162946145d8e4b813f4aad47dc2688cfa38b
Robinson Marquez <robinsonmadrid@outlook.com>
2017-06-21 17:39:21 -0400

    Initial

---
+++ nodejs/sailsjs-trailsjs/README.md
@@ -0,0 +1,3 @@
+# docker-boilerplate
+
+a [Sails](http://sailsjs.org) application
---
+++ os-util-commands/util.ps1
@@ -0,0 +1,7 @@
+﻿
+# Delete all docker container 
+docker ps -a -q | ForEach-Object {docker rm $_}
+"Docker Containers Deleted"
+
+#Delete all docker Images
+docker image ls -a -q | ForEach-Object {docker rmi $_}
