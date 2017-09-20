6438f5a1b2ba01874c5051bcc8e824b13f1d4f55
Robinson <nosnibor1989@gmail.com>
2017-09-20 19:52:38 -0300

    Angular docker service and bash util commands

---
+++ os-util-commands/bash/utils.sh
@@ -0,0 +1,23 @@
+# Delete all docker container 
+docker rm $(docker ps -a -q)
+"Docker Containers Deleted"
+
+# Delete containers according to pattern
+docker ps -a | grep "pattern" | awk '{print $3}' | xargs docker r
+"Docker Containers Deleted"
+
+#Delete containers by filter
+docker rm $(docker ps -a -f status=exited -f status=created -q)
+
+# Stop all containers
+docker stop $(docker ps -a -q)
+
+#Delete all docker Images
+docker rmi $(docker images -a -q)
+"Docker Images Deleted"
+
+#Delete images acording to pattern
+docker images | grep "pattern" | awk '{print $1}' | xargs docker rm
+
+#Delete all networks
+docker rm $(docker network ls -q)
